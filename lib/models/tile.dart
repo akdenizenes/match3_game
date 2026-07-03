c130c3a5105708fdc504b5a8b9b55a6c6687ff2e
Enes Akdeniz <enesakdeniz@posta.mu.edu.tr>
2026-07-03 14:46:31 +0300

    feat: implement core match-3 board generation and swap algorithms

--- lib/models/tile.dart
+++ lib/models/tile.dart
@@ -7,11 +7,13 @@ class Tile {
   TileType type;
   bool isMatched;
   bool isExploding;
+  bool isTargeted; // Used for Propeller targeting crosshair
+  bool isHinted;   // Used for the idle Hint system (Star icon)
   TileType? typeToBecome;
   int row;
   int col;
   
-  // Coordinates for the merge target during a 4+ tile match animation.
+  // Coordinates for the merge target during a 4+ tile match animation
   int? mergeTargetRow; 
   int? mergeTargetCol;
 
@@ -21,10 +23,36 @@ class Tile {
     this.type = TileType.normal,
     this.isMatched = false,
     this.isExploding = false,
+    this.isTargeted = false, 
+    this.isHinted = false,
     this.typeToBecome,
     required this.row,
     required this.col,
     this.mergeTargetRow,
     this.mergeTargetCol,
   });
+
+  // Converts the tile to JSON for saving to local storage
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'color': color.index,
+    'type': type.index,
+    'row': row,
+    'col': col,
+  };
+
+  // Reconstructs the tile object from the saved JSON data
+  factory Tile.fromJson(Map<String, dynamic> json) {
+    return Tile(
+      id: json['id'],
+      color: TileColor.values[json['color']],
+      type: TileType.values[json['type']],
+      row: json['row'],
+      col: json['col'],
+      isTargeted: false, 
+      isHinted: false,
+      isMatched: false,
+      isExploding: false,
+    );
+  }
 }
